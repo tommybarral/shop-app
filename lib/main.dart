31268b65bf3471de1d1b2098ff0a6b2c5ad5684e
Tommybarral <isamutom27@hotmail.com>
2022-02-28 14:14:50 +0100

    stating initial project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Shop app'),
+          centerTitle: true
+      ),
+      body: Center(
+        child: Text('Main page'),
+      ),
+    );
+  }
+}
